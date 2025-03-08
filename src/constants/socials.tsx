3702a4a149ed0aa300f15b0de36c13ca03cf13a9
BossiaCI <bossiadaha@gmail.com>
2025-03-08 17:22:38 +0100

    initialize portfolio repository

---
+++ src/constants/socials.tsx
@@ -0,0 +1,29 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandGithub
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://twitter.com/#",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://linkedin.com/in/#",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://youtube.com/#",
+    label: "YouTube",
+    icon: IconBrandYoutube,
+  },
+  {
+    href: "https://github.com/BossiaCI",
+    label: "GitHub",
+    icon: IconBrandGithub,
+  },
+];
